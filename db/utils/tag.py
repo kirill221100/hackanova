28f2d36433b51bd6f01f08963822ea79d0f87c08
Gleb <calebras@mail.ru>
2024-05-25 19:23:35 +0300

    Save changes before switching branches

--- db/utils/tag.py
+++ db/utils/tag.py
@@ -1,9 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, literal
 from sqlalchemy.orm import selectinload
 from db.models.tag import Tag
 from typing import List
 from fastapi import HTTPException
+
+from db.models.user import User
 
 
 async def get_tags_by_names(names: List[str], session: AsyncSession):
@@ -21,3 +23,14 @@ async def create_tag(name: str, session: AsyncSession):
     session.add(tag)
     await session.commit()
     return tag
+
+
+async def update_tags_on_user(user_id: int, tags: List[str], session: AsyncSession):
+    result = await session.execute(select(User).filter(User.id == literal(user_id)))
+    user = result.scalar_one_or_none()
+    # Get the list of tags from the database
+    tags = List[str]
+    # Assign the tags to the user
+    user.tags.extend(tags)
+    await session.commit()
+    return user.tags
--- routes/tag.py
+++ routes/tag.py
@@ -2,10 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List, Optional
 from db.db_setup import get_session
-from db.utils.tag import create_tag, get_all_tags
+from db.utils.tag import create_tag, get_all_tags, update_tags_on_user
 from db.utils.team import get_team_by_tags
 from schemes.tag import TagResponse, GetTeamByTagsSchemeResponse
-
 
 tag_router = APIRouter()
 
@@ -23,3 +22,8 @@ async def get_team_by_tags_path(tags: List[str] = Query(None), session: AsyncSes
 @tag_router.get("/get-tag-list", response_model=List[TagResponse])
 async def get_tag_list_path(session: AsyncSession = Depends(get_session)):
     return await get_all_tags(session)
+
+
+@tag_router.post("/user/{user_id}/add_tags", response_model=List[str])
+async def add_tags_to_user(user_id: int, tags: List[str], session: AsyncSession = Depends(get_session)):
+    return await update_tags_on_user(user_id=user_id, tags=tags, session=session)
